ec555705844a0140a20358873fc8fd9ea4497282
Zakary Bliss <zakarybliss@ucsb.edu>
2020-05-04 11:45:24 -0700

    4 jellyfish rendered on a marker

---
+++ aframe/app.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2019 8th Wall, Inc.
+//
+// app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
+// is loaded, and before body.html is loaded.
